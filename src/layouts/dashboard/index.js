8333bef6b7edc1bf1b5af83d64bbae0363cc4361
AminahAkhtar <aminah30akhtar3a@gmail.com>
2024-07-08 14:19:06 +0500

    dashboard-complete

---
+++ src/layouts/dashboard/index.js
@@ -0,0 +1,299 @@
+/**
+=========================================================
+* Material Dashboard 2 React - v2.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+Coded by www.creative-tim.com
+
+ =========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+*/
+
+// @mui material components
+import Grid from "@mui/material/Grid";
+
+// Material Dashboard 2 React components
+import MDBox from "components/MDBox";
+import React, { useEffect, useState } from "react";
+// Material Dashboard 2 React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import ReportsBarChart from "examples/Charts/BarCharts/ReportsBarChart";
+import ReportsLineChart from "examples/Charts/LineCharts/ReportsLineChart";
+import ComplexStatisticsCard from "examples/Cards/StatisticsCards/ComplexStatisticsCard";
+// Data
+import reportsBarChartData from "layouts/dashboard/data/reportsBarChartData";
+import reportsLineChartData from "layouts/dashboard/data/reportsLineChartData";
+import MDTypography from "components/MDTypography";
+// Dashboard components
+import Projects from "layouts/dashboard/components/Projects";
+import OrdersOverview from "layouts/dashboard/components/OrdersOverview";
+import BatteryCharging30OutlinedIcon from "@mui/icons-material/BatteryCharging30Outlined";
+import TwoWheelerOutlinedIcon from "@mui/icons-material/TwoWheelerOutlined";
+import MonitorHeartOutlinedIcon from "@mui/icons-material/MonitorHeartOutlined";
+// function Dashboard() {
+//   const { sales, tasks } = reportsLineChartData;
+//   const [eve1Data, setEve1Data] = useState({ totalCycles: 0 });
+//   const [eve2Data, setEve2Data] = useState({ totalCycles: 0, totalSpikes: 0 });
+//   const [chartData, setChartData] = useState({
+//     labels: [],
+//     datasets: [{
+//       label: "Cell Difference",
+//       data: [],
+//     }]
+//   });
+//   useEffect(() => {
+//     fetch("http://localhost:5000/eve1")
+//       .then((response) => {
+//         return response.json();
+//       })
+//       .then((data) => {
+//         setEve1Data({ totalCycles: data.eve1_total_cycles });
+//       })
+//       .catch((error) => console.error("Error fetching eve1 data:", error));
+//     fetch("http://localhost:5000/eve2")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setEve2Data({ totalCycles: data.eve2_total_cycles, totalSpikes: data.eve2_total_spikes });
+//       })
+//       .catch((error) => console.error("Error fetching eve2 data:", error));
+//     fetch("http://localhost:5000/eve2_cell_diff")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         const cycleNumbers = data.map((entry) => entry["Cycle Number"]);
+//         const cellDifferences = data.map((entry) => entry["Cell Difference"]);
+//         setChartData({
+//           labels: cycleNumbers,
+//           datasets: [{
+//             label: "Cell Difference",
+//             data: cellDifferences,
+//             fill: false,
+//             borderColor: "rgb(75, 192, 192)",
+//             tension: 0.1
+//           }]
+//         });
+//       })
+//       .catch((error) => console.error("Error fetching data:", error));
+//   }, []);
+//   return (
+//     <DashboardLayout>
+//       <DashboardNavbar />
+//       <MDBox py={3}>
+//         <Grid container spacing={3}>
+//           <Grid item xs={12} md={6} lg={3}>
+//             <MDBox mb={1.5}>
+//               <ComplexStatisticsCard
+//                 color="dark"
+//                 icon={<BatteryCharging30OutlinedIcon />}
+//                 title="Total Batteries Analysed"
+//                 count={2}
+//               />
+//             </MDBox>
+//           </Grid>
+//           <Grid item xs={12} md={6} lg={3}>
+//             <MDBox mb={1.5}>
+//               <ComplexStatisticsCard
+//                 icon={<TwoWheelerOutlinedIcon />}
+//                 title="EVE1 - Total Cycles"
+//                 count={/* prettier-ignore */ eve1Data.totalCycles}
+//               />
+//             </MDBox>
+//           </Grid>
+//           <Grid item xs={12} md={6} lg={3}>
+//             <MDBox mb={1.5}>
+//               <ComplexStatisticsCard
+//                 color="success"
+//                 icon={<TwoWheelerOutlinedIcon />}
+//                 title="EVE2 - Total Cycles"
+//                 count={/* prettier-ignore */ eve2Data.totalCycles}
+//               />
+//             </MDBox>
+//           </Grid>
+//           <Grid item xs={12} md={6} lg={3}>
+//             <MDBox mb={1.5}>
+//               <ComplexStatisticsCard
+//                 color="primary"
+//                 icon={<MonitorHeartOutlinedIcon />}
+//                 title="EVE2 - Total Spikes"
+//                 count={/* prettier-ignore */ eve2Data.totalSpikes}
+//               />
+//             </MDBox>
+//           </Grid>
+//         </Grid>
+//         <MDBox mt={4.5}>
+//           <Grid container spacing={3}>
+//             <Grid item xs={12} md={6} lg={4}>
+//               <MDBox mb={3}>
+//                 <ReportsBarChart
+//                   color="info"
+//                   title="website views"
+//                   description="Last Campaign Performance"
+//                   date="campaign sent 2 days ago"
+//                   chart={reportsBarChartData}
+//                 />
+//               </MDBox>
+//             </Grid>
+//             <Grid item xs={12} md={6} lg={4}>
+//               <MDBox mb={3}>
+//                 <ReportsLineChart
+//                   color="success"
+//                   title="daily sales"
+//                   description={
+//                     <>
+//                       (<strong>+15%</strong>) increase in today sales.
+//                     </>
+//                   }
+//                   date="updated 4 min ago"
+//                   chart={chartData}
+//                 />
+//               </MDBox>
+//             </Grid>
+//             <Grid item xs={12} md={6} lg={4}>
+//               <MDBox mb={3}>
+//                 <ReportsLineChart
+//                   color="dark"
+//                   title="completed tasks"
+//                   description="Last Campaign Performance"
+//                   date="just updated"
+//                   chart={tasks}
+//                 />
+//               </MDBox>
+//             </Grid>
+//           </Grid>
+//         </MDBox>
+//       </MDBox>
+//       <Footer />
+//     </DashboardLayout>
+//   );
+// }
+
+// export default Dashboard;
+
+function Dashboard() {
+  const { sales, tasks } = reportsLineChartData;
+  const [eve1Data, setEve1Data] = useState({ totalCycles: 0 });
+  const [eve2Data, setEve2Data] = useState({ totalCycles: 0, totalSpikes: 0 });
+  const [imageSrc1, setImageSrc1] = useState("");
+  const [imageSrc2, setImageSrc2] = useState("");
+  useEffect(() => {
+    fetch("http://localhost:5000/eve1")
+      .then((response) => response.json())
+      .then((data) => {
+        setEve1Data({ totalCycles: data.eve1_total_cycles });
+      })
+      .catch((error) => console.error("Error fetching eve1 data:", error));
+
+    fetch("http://localhost:5000/eve2")
+      .then((response) => response.json())
+      .then((data) => {
+        setEve2Data({ totalCycles: data.eve2_total_cycles, totalSpikes: data.eve2_total_spikes });
+      })
+      .catch((error) => console.error("Error fetching eve2 data:", error));
+    fetch("http://localhost:5000/eve1_cell_diff")
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setImageSrc1(url);
+      })
+      .catch((error) => console.error("Error fetching graph:", error));
+    fetch("http://localhost:5000/compare")
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
+        setImageSrc2(url);
+      })
+      .catch((error) => console.error("Error fetching graph:", error));
+  }, []);
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox py={3}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="dark"
+                icon={<BatteryCharging30OutlinedIcon />}
+                title="Total Batteries Analysed"
+                count={2}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                icon={<TwoWheelerOutlinedIcon />}
+                title="EVE1 - Total Cycles"
+                count={/* prettier-ignore */ eve1Data.totalCycles}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="success"
+                icon={<TwoWheelerOutlinedIcon />}
+                title="EVE2 - Total Cycles"
+                count={/* prettier-ignore */ eve2Data.totalCycles}
+              />
+            </MDBox>
+          </Grid>
+          <Grid item xs={12} md={6} lg={3}>
+            <MDBox mb={1.5}>
+              <ComplexStatisticsCard
+                color="primary"
+                icon={<MonitorHeartOutlinedIcon />}
+                title="EVE2 - Total Spikes"
+                count={/* prettier-ignore */ eve2Data.totalSpikes}
+              />
+            </MDBox>
+          </Grid>
+        </Grid>
+        <MDBox mt={2} ml={20}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <MDTypography variant="h4" fontWeight="medium" textTransform="capitalize">
+                  EVE1 Cycles And Cell Difference Analysis
+                </MDTypography>
+                <img
+                  src={imageSrc1}
+                  alt="Cell Difference Graph"
+                  style={{
+                    borderRadius: "8px",
+                  }}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+        <MDBox mt={4.5} ml={20}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} md={6} lg={4}>
+              <MDBox mb={3}>
+                <MDTypography variant="h4" fontWeight="medium" textTransform="capitalize">
+                  EVE1 And EVE2 Comparison
+                </MDTypography>
+                <img
+                  src={imageSrc2}
+                  alt="Cell Difference Graph"
+                  style={{
+                    borderRadius: "8px",
+                  }}
+                />
+              </MDBox>
+            </Grid>
+          </Grid>
+        </MDBox>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+export default Dashboard;
---
+++ src/layouts/tables/index.js
@@ -0,0 +1,421 @@
+// /**
+// =========================================================
+// * Material Dashboard 2 React - v2.2.0
+// =========================================================
+
+// * Product Page: https://www.creative-tim.com/product/material-dashboard-react
+// * Copyright 2023 Creative Tim (https://www.creative-tim.com)
+
+// Coded by www.creative-tim.com
+
+//  =========================================================
+
+// * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// */
+
+// // @mui material components
+// import Grid from "@mui/material/Grid";
+// import Card from "@mui/material/Card";
+
+// // Material Dashboard 2 React components
+// import MDBox from "components/MDBox";
+// import MDTypography from "components/MDTypography";
+
+// // Material Dashboard 2 React example components
+// import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+// import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+// import Footer from "examples/Footer";
+// import DataTable from "examples/Tables/DataTable";
+
+// // Data
+// import authorsTableData from "layouts/tables/data/authorsTableData";
+// import projectsTableData from "layouts/tables/data/projectsTableData";
+
+// function Tables() {
+//   const { columns, rows } = authorsTableData();
+//   const { columns: pColumns, rows: pRows } = projectsTableData();
+
+//   return (
+//     <DashboardLayout>
+//       <DashboardNavbar />
+//       <MDBox pt={6} pb={3}>
+//         <Grid container spacing={6}>
+//           <Grid item xs={12}>
+//             <Card>
+//               <MDBox
+//                 mx={2}
+//                 mt={-3}
+//                 py={3}
+//                 px={2}
+//                 variant="gradient"
+//                 bgColor="info"
+//                 borderRadius="lg"
+//                 coloredShadow="info"
+//               >
+//                 <MDTypography variant="h6" color="white">
+//                   EVE1 Data
+//                 </MDTypography>
+//               </MDBox>
+//               <MDBox pt={3}>
+//                 <DataTable
+//                   table={{ columns, rows }}
+//                   isSorted={false}
+//                   entriesPerPage={false}
+//                   showTotalEntries={false}
+//                   noEndBorder
+//                 />
+//               </MDBox>
+//             </Card>
+//           </Grid>
+//           <Grid item xs={12}>
+//             <Card>
+//               <MDBox
+//                 mx={2}
+//                 mt={-3}
+//                 py={3}
+//                 px={2}
+//                 variant="gradient"
+//                 bgColor="info"
+//                 borderRadius="lg"
+//                 coloredShadow="info"
+//               >
+//                 <MDTypography variant="h6" color="white">
+//                   EVE2 Data
+//                 </MDTypography>
+//               </MDBox>
+//               <MDBox pt={3}>
+//                 <DataTable
+//                   table={{ columns: pColumns, rows: pRows }}
+//                   isSorted={false}
+//                   entriesPerPage={false}
+//                   showTotalEntries={false}
+//                   noEndBorder
+//                 />
+//               </MDBox>
+//             </Card>
+//           </Grid>
+//         </Grid>
+//       </MDBox>
+//       <Footer />
+//     </DashboardLayout>
+//   );
+// }
+
+// export default Tables;
+
+import React, { useEffect, useState } from "react";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+import DashboardNavbar from "examples/Navbars/DashboardNavbar";
+import Footer from "examples/Footer";
+import DataTable from "examples/Tables/DataTable";
+
+function Tables() {
+  const [eve1Data, setEve1Data] = useState({ columns: [], rows: [] });
+  const [eve2Data, setEve2Data] = useState({ columns: [], rows: [] });
+  const [batteryHealthData, setBatteryHealthData] = useState({ columns: [], rows: [] });
+  useEffect(() => {
+    fetch("http://localhost:5000/eve1_data")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API response data:", data); // Debug: Log the API response
+        const columns = [
+          { Header: "Seconds", accessor: "Seconds", align: "center" },
+          { Header: "Voltage", accessor: "Voltage", align: "center" },
+          { Header: "Current", accessor: "Current", align: "center" },
+          { Header: "SOC", accessor: "SOC", align: "center" },
+          { Header: "Charge MOS", accessor: "Charge_MOS", align: "center" },
+          { Header: "Discharge MOS", accessor: "Discharge_MOS", align: "center" },
+          { Header: "Max Temp", accessor: "Max_Temp", align: "center" },
+          { Header: "Cell Diff", accessor: "Cell_Diff", align: "center" },
+          { Header: "Date", accessor: "Date", align: "center" },
+        ];
+
+        const rows = data.map((item) => ({
+          Seconds: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Seconds}
+            </MDTypography>
+          ),
+          Voltage: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Voltage}
+            </MDTypography>
+          ),
+          Current: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Current}
+            </MDTypography>
+          ),
+          SOC: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.SOC}
+            </MDTypography>
+          ),
+          Charge_MOS: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Charge MOS"]}
+            </MDTypography>
+          ),
+          Discharge_MOS: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Discharge MOS"]}
+            </MDTypography>
+          ),
+          Max_Temp: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Max Temp"]}
+            </MDTypography>
+          ),
+          Cell_Diff: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Cell Diff"]}
+            </MDTypography>
+          ),
+          Date: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Date}
+            </MDTypography>
+          ),
+        }));
+
+        setEve1Data({ columns, rows });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/eve2_data")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API response data:", data); // Debug: Log the API response
+        const columns = [
+          { Header: "Seconds", accessor: "Seconds", align: "center" },
+          { Header: "Voltage", accessor: "Voltage", align: "center" },
+          { Header: "Current", accessor: "Current", align: "center" },
+          { Header: "SOC", accessor: "SOC", align: "center" },
+          { Header: "Charge MOS", accessor: "Charge_MOS", align: "center" },
+          { Header: "Discharge MOS", accessor: "Discharge_MOS", align: "center" },
+          { Header: "Max Temp", accessor: "Max_Temp", align: "center" },
+          { Header: "Cell Diff", accessor: "Cell_Diff", align: "center" },
+          { Header: "Date", accessor: "Date", align: "center" },
+        ];
+
+        const rows = data.map((item) => ({
+          Seconds: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Seconds}
+            </MDTypography>
+          ),
+          Voltage: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Voltage}
+            </MDTypography>
+          ),
+          Current: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Current}
+            </MDTypography>
+          ),
+          SOC: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.SOC}
+            </MDTypography>
+          ),
+          Charge_MOS: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Charge MOS"]}
+            </MDTypography>
+          ),
+          Discharge_MOS: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Discharge MOS"]}
+            </MDTypography>
+          ),
+          Max_Temp: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Max Temp"]}
+            </MDTypography>
+          ),
+          Cell_Diff: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Cell Diff"]}
+            </MDTypography>
+          ),
+          Date: (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item.Date}
+            </MDTypography>
+          ),
+        }));
+
+        setEve2Data({ columns, rows });
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/data_compare")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("API response data (data_compare):", data); // Debug: Log the API response
+        // prettier-ignore
+        const columns = [
+          { Header: "Cycle Number", accessor: "Cycle Number", align: "center" },
+          { Header: "EVE1 Last Cell Diff", accessor: "EVE1 Last Cell Diff", align: "center" },
+          { Header: "EVE2 Last Cell Diff", accessor: "EVE2 Last Cell Diff", align: "center" },
+          { Header: "Battery Health", accessor: "Battery Health", align: "center" },
+          { Header: "Total Spikes in EVE2", accessor: "Total Spikes in EVE2", align: "center" },
+          // { Header: "Cell Diff at Spikes in EVE2", accessor: "Cell Diff at Spikes in EVE2", align: "center" },
+        ];
+
+        const rows = data.map((item) => ({
+          "Cycle Number": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Cycle Number"]}
+            </MDTypography>
+          ),
+          "EVE1 Last Cell Diff": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["EVE1 Last Cell Diff"]}
+            </MDTypography>
+          ),
+          "EVE2 Last Cell Diff": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["EVE2 Last Cell Diff"]}
+            </MDTypography>
+          ),
+          "Battery Health": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Battery Health"]}
+            </MDTypography>
+          ),
+          "Total Spikes in EVE2": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Total Spikes in EVE2"]}
+            </MDTypography>
+          ),
+          "Cell Diff at Spikes in EVE2": (
+            <MDTypography variant="caption" color="text" fontWeight="medium">
+              {item["Cell Diff at Spikes in EVE2"]}
+            </MDTypography>
+          ),
+        }));
+
+        setBatteryHealthData({ columns, rows });
+      })
+      .catch((error) => console.error("Error fetching data_compare:", error));
+  }, []);
+
+  return (
+    <DashboardLayout>
+      <DashboardNavbar />
+      <MDBox pt={6} pb={3}>
+        <Grid container spacing={6}>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  EVE1 Data
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={eve1Data}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  EVE2 Data
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={eve2Data}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <Card>
+              <MDBox
+                mx={2}
+                mt={-3}
+                py={3}
+                px={2}
+                variant="gradient"
+                bgColor="info"
+                borderRadius="lg"
+                coloredShadow="info"
+              >
+                <MDTypography variant="h6" color="white">
+                  Health Comparison
+                </MDTypography>
+              </MDBox>
+              <MDBox pt={3}>
+                <DataTable
+                  table={batteryHealthData}
+                  isSorted={false}
+                  entriesPerPage={false}
+                  showTotalEntries={false}
+                  noEndBorder
+                />
+              </MDBox>
+            </Card>
+          </Grid>
+        </Grid>
+      </MDBox>
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Tables;
